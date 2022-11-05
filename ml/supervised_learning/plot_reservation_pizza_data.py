2fc57c0cd41a0f400a395bbcf9c7ddfc90db00f4
DevOpsThinh <nguyentruongthinhvn2020@gmail.com>
2022-11-05 23:51:57 +0700

    Dive into machine learning: Supervised Learning

---
+++ ml/supervised_learning/plot_reservation_pizza_data.py
@@ -0,0 +1,19 @@
+# Learner: Nguyen Truong Thinh
+# Contact me: nguyentruongthinhvn2020@gmail.com || +84393280504
+#
+# Topic: Supervised Learning
+# Plot the reservations/ pizzas dataset
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+sns.set()
+plt.axis([0, 50, 0, 50])
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.xlabel("Reservations", fontsize=30)
+plt.ylabel("Pizzas", fontsize=30)
+X, Y = np.loadtxt("../../fundamentals/datasets/pizza.txt", skiprows=1, unpack=True)
+plt.plot(X, Y, "bo")
+plt.show()
