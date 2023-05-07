2d0c6e594d16ef9817f8eabe51c2d5baf3a675e0
DevOpsThinh <nguyentruongthinhvn2020@gmail.com>
2023-05-07 16:55:47 +0700

    Deep Learning: Defeating over-fitting via L1, L2 Regularization, collect more training data, simplifying the model & increasing Learning rate.

--- ml/deep_learning/neural_network_keras/decision_boundaries/decision_boundary_2dimensional.py
+++ ml/deep_learning/neural_network_keras/decision_boundaries/decision_boundary_2dimensional.py
@@ -7,7 +7,26 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from matplotlib.colors import ListedColormap
+
+
+def plot(history):
+    """
+    A utility function that plots the training loss and validation loss from a Keras history object.
+    :param history: A Keras history object.
+    :return: None
+    """
+    sns.set()
+    plt.plot(history.history['loss'], label='Training set', color='blue', linestyle='-')
+    plt.plot(history.history["val_loss"], label='Validation set', color='green', linestyle='--')
+    plt.xlabel("Epochs", fontsize=30)
+    plt.ylabel("Loss", fontsize=30)
+    plt.xlim(0, len(history.history['loss']))
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.legend(fontsize=30)
+    plt.show()
 
 
 def plot_boundary(model, points):
---
+++ ml/deep_learning/neural_network_keras/overfitting/defeating_overfitting_regularization.py
@@ -0,0 +1,50 @@
+# Learner: Nguyen Truong Thinh
+# Contact me: nguyentruongthinhvn2020@gmail.com || +84393280504
+#
+# Topic: Deep Learning: The Echidna dataset.
+#           Building the echidna - four-layered neural network with L1 regularization
+
+import os
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import RMSprop
+from keras.regularizers import l1
+from keras.utils import to_categorical
+
+from ml.deep_learning.neural_network_keras.decision_boundaries import decision_boundary_2dimensional as boundary
+from ml.util import load_seeded_shuffled_text_dataset
+
+# Load the Echidna dataset
+
+current_directory = os.path.dirname(__file__)
+text_dataset = os.path.join(current_directory, "echidna.txt")
+
+X, Y = load_seeded_shuffled_text_dataset(text_dataset)
+
+X_train, X_validation, X_test = np.split(X, 3)
+Y_train, Y_validation, Y_test = np.split(Y, 3)
+
+Y_train_encoded = to_categorical(Y_train)
+Y_validation_encoded = to_categorical(Y_validation)
+
+# Create a sequential model
+model = Sequential()
+model.add(Dense(100, activation='sigmoid', activity_regularizer=l1(0.002)))
+model.add(Dense(30, activation='sigmoid', activity_regularizer=l1(0.0004)))
+model.add(Dense(2, activation='softmax'))
+
+#  Compile a model
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.001), metrics=['accuracy'])
+
+# Train a network
+history = model.fit(X_train, Y_train_encoded, validation_data=(X_validation, Y_validation_encoded), epochs=30000,
+                    batch_size=25)
+# Epoch 30000/30000 12/12 [==============================] - 0s 2ms/step - loss: 0.0036 - accuracy: 1.0000 -
+# val_loss: 0.7884 - val_accuracy: 0.8877 37813/37813 [==============================] - 17s 441us/step
+
+# Draw a decision boundary
+boundary.show(model, X_train, Y_train, title="Training set")
+boundary.show(model, X_validation, Y_validation_encoded, title="Validation set")
+boundary.plot(history)
